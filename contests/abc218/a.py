caf686a2f8e9b8066c3b95bd74896563f5eac885
ganopippi <meunity613@gmail.com>
2021-09-12 00:01:39 +0900

    20210912 ABC218

---
+++ contests/abc218/a.py
@@ -0,0 +1,8 @@
+n = int(input())
+s = input().split()
+
+
+if s[0][n-1] == 'o':
+  print('Yes')
+else:
+  print('No')
---
+++ contests/abc218/b.py
@@ -0,0 +1,58 @@
+l = list(map(int, input().split()))
+r = []
+
+for n in l:
+  if n == 1:
+    r.append('a')
+  elif n == 2:
+    r.append('b')
+  elif n == 3:
+    r.append('c')
+  elif n == 4:
+    r.append('d')
+  elif n == 5:
+    r.append('e')
+  elif n == 6:
+    r.append('f')
+  elif n == 7:
+    r.append('g')
+  elif n == 8:
+    r.append('h')
+  elif n == 9:
+    r.append('i')
+  elif n == 10:
+    r.append('j')
+  elif n == 11:
+    r.append('k')
+  elif n == 12:
+    r.append('l')
+  elif n == 13:
+    r.append('m')
+  elif n == 14:
+    r.append('n')
+  elif n == 15:
+    r.append('o')
+  elif n == 16:
+    r.append('p')
+  elif n == 17:
+    r.append('q')
+  elif n == 18:
+    r.append('r')
+  elif n == 19:
+    r.append('s')
+  elif n == 20:
+    r.append('t')
+  elif n == 21:
+    r.append('u')
+  elif n == 22:
+    r.append('v')
+  elif n == 23:
+    r.append('w')
+  elif n == 24:
+    r.append('x')
+  elif n == 25:
+    r.append('y')
+  elif n == 26:
+    r.append('z')
+
+print(''.join(r))
